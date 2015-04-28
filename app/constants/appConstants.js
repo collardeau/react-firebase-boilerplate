5c4f166f64d595df7b6ed78fe25be974045dc4df
Thomas Collardeau <thomas@collardeau.com>
2015-04-28 22:00:09 +0200

    remove useless constants

--- app/constants/appConstants.js
+++ app/constants/appConstants.js
@@ -1,13 +1,9 @@
 var appConstants = {
-  FIREBASE_HOST: "https://tc-react-boilerplate.firebaseio.com/",
-  CHANGE_USER: "CHANGE_USER",
-  GET_TIMELINE: "GET_TIMELINE",
-  GET_EVENT: "GET_EVENT",
-  ADD_EVENT: "ADD_EVENT",
-  //ADD_ITEM: "ADD_ITEM",
-  REMOVE_ITEM: "REMOVE_ITEM",
+
+    FIREBASE_HOST: "https://tc-react-boilerplate.firebaseio.com/",
     GET_DATA: "GET_DATA",
-    ADD_ITEM: "ADD_ITEM"
+    ADD_ITEM: "ADD_ITEM",
+    REMOVE_ITEM: "REMOVE_ITEM"
 };
 
 module.exports = appConstants;
