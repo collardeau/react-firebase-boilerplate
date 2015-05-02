a6a83f190c3f90a0d889d9f9d07552d7b787abe7
Thomas Collardeau <thomas@collardeau.com>
2015-05-02 15:52:52 +0200

    bundle css into own bundle (webpack)

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var neat = require('node-neat').includePaths;
 var sassNeatPaths = require("node-neat").includePaths.map(function(sassPath) {
     return "includePaths[]=" + sassPath;
@@ -26,18 +27,18 @@ var config = {
     module: {
         loaders: [
             { test: /\.(js|jsx)$/, loaders: ['react-hot', 'jsx', 'babel'], exclude: /node_modules/},
-            { test: /\.css$/, loaders: ['style', 'css'] },
-            { test: /\.scss$/, loaders: ['style', 'css', 'sass?' + sassNeatPaths] }
-
+            { test: /\.scss$/, loaders: [ExtractTextPlugin.extract("css"), 'css', 'sass?' + sassNeatPaths] },
+            { test: /\.css$/, loader: ExtractTextPlugin.extract("css") }
         ]
     },
 
     plugins: [
         new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js"),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-        // If you want to minify everything
-        // new webpack.optimize.UglifyJsPlugin()
+        new webpack.NoErrorsPlugin(),
+        new ExtractTextPlugin("bundle.css"),
+        // If you want to minify everything (including css bundle), perhaps remove react-hot?
+        //new webpack.optimize.UglifyJsPlugin()
     ]
 };
 
