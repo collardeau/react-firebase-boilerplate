d717816e4b95076904c4006669a5a5f613159734
Thomas Collardeau <thomas@collardeau.com>
2015-05-06 15:17:23 +0200

    enable firebase auth

--- app/components/login/Register.js
+++ app/components/login/Register.js
@@ -5,10 +5,13 @@ class Register extends React.Component {
 
     constructor(){
         super();
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleRegister = this.handleRegister.bind(this);
+        this.handleLogout = this.handleLogout.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
 
-    handleSubmit(e){
+    handleRegister(e){
+        console.log("handling register");
         e.preventDefault();
         let email = this.refs.email.getDOMNode().value;
         let pw = this.refs.pw.getDOMNode().value;
@@ -19,17 +22,36 @@ class Register extends React.Component {
         this.refs.pw.getDOMNode().value = "";
     }
 
+    handleLogin(e){
+        console.log("handling login");
+        e.preventDefault();
+        let email = this.refs.email.getDOMNode().value;
+        let pw = this.refs.pw.getDOMNode().value;
+        firebaseUtils.loginWithPw({email: email, password: pw});
+        this.refs.email.getDOMNode().value = "";
+        this.refs.pw.getDOMNode().value = "";
+
+    }
+
+    handleLogout(e){
+        e.preventDefault();
+        console.log("handling logout");
+        firebaseUtils.logout();
+    }
+
     render() {
         return (
             <div className="container">
                 <h1>Register</h1>
 
-                <form onSubmit={this.handleSubmit}>
+                <form>
                     <label>Email</label>
                     <input type="text" ref="email" placeholder="Email"/>
                     <label>Password</label>
-                    <input type="text" ref="pw" placeholder="Password"/>
-                    <button type="submit" className="btn btn-action">Login</button>
+                    <input type="password" ref="pw" placeholder="Password"/>
+                    <button onClick={this.handleRegister} className="btn btn-action">Register</button>
+                    <button onClick={this.handleLogin} className="btn btn-action">Login</button>
+                    <button onClick={this.handleLogout} className="btn-alert">Log Out</button>
                 </form>
             </div>
         )
--- app/utils/FirebaseUtils.js
+++ app/utils/FirebaseUtils.js
@@ -2,11 +2,24 @@ var Firebase = require('firebase');
 var appConstants = require('../constants/appConstants');
 var ref = new Firebase(appConstants.FIREBASE_HOST);
 
+var addNewUserToFB = function(newUser){
+    ref.child('user').child(newUser.uid).set(newUser);
+};
+
+//function authDataCallback(authData) {
+//    if (authData) {
+//        console.log("User " + authData.uid + " is logged in with " + authData.provider);
+//    } else {
+//        console.log("User is logged out");
+//    }
+//}
+
 var firebaseUtils = {
 
     homeInstance: function () {
         return new Firebase(appConstants.FIREBASE_HOST);
     },
+
     addItem: function (item) {
         this.homeInstance().child("list").push(item);
     },
@@ -15,7 +28,8 @@ var firebaseUtils = {
         this.homeInstance().child("list").child(key).remove();
     },
 
-    createUser: function(user, cb) {
+    createUser: function(user) {
+        console.log("creating user");
         ref.createUser(user, function(err) {
             if (err) {
                 switch (err.code) {
@@ -23,38 +37,44 @@ var firebaseUtils = {
                         console.log("The new user account cannot be created because the email is already in use.");
                         break;
                     case "INVALID_EMAIL":
-                        console.log("The specified email is not a valid email.");
+                        console.log("The specifiedsdfs email is not a valid email.");
                         break;
                     default:
                         console.log("Error creating user:", err);
                 }
             } else {
-                //this.loginWithPW(user, function(authData){
-                //    addNewUserToFB({
-                //        email: user.email,
-                //        uid: authData.uid,
-                //        token: authData.token
-                //    });
-                //}, cb);
-                console.log("created a user!");
+                this.loginWithPw(user, function(authData){
+                    addNewUserToFB({
+                        email: user.email,
+                        uid: authData.uid,
+                        token: authData.token
+                    });
+                });
             }
         }.bind(this));
     },
-    //
-    //loginWithPW: function(userObj, cb, cbOnRegister){
-    //    ref.authWithPassword(userObj, function(err, authData){
-    //        if(err){
-    //            console.log('Error on login:', err.message);
-    //            cbOnRegister && cbOnRegister(false);
-    //        } else {
-    //            authData.email = userObj.email;
-    //            cachedUser = authData;
-    //            cb(authData);
-    //            this.onChange(true);
-    //            cbOnRegister && cbOnRegister(true);
-    //        }
-    //    }.bind(this));
-    //},
+
+    loginWithPw: function(user, cb){
+        ref.authWithPassword({
+            email    : user.email,
+            password : user.password
+        }, function(error, authData) {
+            if (error) {
+                console.log("Login Failed!", error);
+            } else {
+                console.log("Authenticated successfully with payload:", authData);
+                cb && cb(authData);   // adding user to db after registering
+            }
+        })
+    },
+
+    logout: function(){
+        ref.unauth(function(foo){
+            console.log(foo);
+            console.log("logged out");
+        });
+        //console.log(auth.uid)
+    },
 
     toArray: function (obj) {
         var arr = [];
