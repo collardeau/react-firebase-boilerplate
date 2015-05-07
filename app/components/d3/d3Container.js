4addee2ee8b4f5dbec7d0575a501349b7320d3a5
Thomas Collardeau <thomas@collardeau.com>
2015-05-07 22:23:35 +0200

    messing around with proportions

--- app/components/d3/d3Container.js
+++ app/components/d3/d3Container.js
@@ -5,24 +5,34 @@ class D3Container extends React.Component {
 
     componentDidMount() {
 
-        let w = 100, h = 100;
+        let dataset = [5, 15, 40, 60, 75, 90];
 
-        let svg = d3.select(".d3-container")
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h);
+        let w = 400, h = 50;
 
-        svg.append("circle")
-            .attr("cx", w/4)
-            .attr("cy", w/4)
-            .attr("r", 25)
-            .style("fill", "purple");
+        let svg = d3.select(".d3-container").append("svg").attr("width", w).attr("height", h);
 
-        svg.append("circle")
-            .attr("cx", w/4*3)
-            .attr("cy", w/4*3)
-            .attr("r", 25)
-            .style("fill", "blue")
+        svg.append("line").
+            attr({
+                x1: 0, x2: w,
+                y1: 20, y2: 20,
+                "id": "line"
+            })
+            .style({
+                stroke: "black",
+                "stroke-width": 10
+            });
+
+        svg.selectAll("circle")
+            .data(dataset)
+            .enter()
+            .append('circle')
+                .attr({
+                    cx: (d,i) => d*w/100,
+                    cy: 20,
+                    r: 10,
+                    "class": "circle"
+                })
+            .style("fill", "orange");
     }
 
     render() {
@@ -31,6 +41,7 @@ class D3Container extends React.Component {
             <div className="container d3-container">
                 <h1>D3 Action</h1>
             </div>
+
         )
     }
 
