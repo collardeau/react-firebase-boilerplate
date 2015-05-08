c4d7f879fa3210c7e6c3260df539af83c0606088
Thomas Collardeau <thomas@collardeau.com>
2015-05-08 16:48:59 +0200

    play with d3 transitions

--- app/components/d3/Line.js
+++ app/components/d3/Line.js
@@ -2,60 +2,26 @@ const React = require('react');
 
 class Line extends React.Component {
 
-    constructor () {
-        super();
-        this.state = {
-            x1:"",
-            x2:"100",
-            y1:"100",
-            y2:"20",
-            stroke:"black",
-            strokeWidth:"10"
-            }
-    }
-
-    componentDidMount() {
-        this.setState({
-            x1:parseInt(this.props.x1),
-            x2:"100",
-            y1:"100",
-            y2:"20",
-            stroke:"black",
-            strokeWidth:"10"
-        });
-    }
-
     handleClick() {
-        var i = 1;
-        var foo = setInterval(() => {
-            //console.log("Hello");
-            this.setState({
-                x1: this.state.x1 += 1
-            });
-            i++;
-        }, 1000/60);
-
-        setTimeout(function(){ clearInterval(foo)}, 6000);
-        //this.props.stroke = "yellow";
-        //this.forceUpdate();
-
+        console.log("click");
     }
 
     render() {
 
         return (
-            <svg width="500" height="200">
 
-                <line onClick={this.handleClick.bind(this)}
-                      x1={this.state.x1}
-                      x2={this.props.x2}
-                      y1={this.props.y1}
-                      y2={this.props.y2}
-                      stroke={this.props.stroke}
-                      strokeWidth={this.props.strokeWidth}
-                    />
-
-            </svg>
+            <div>
+                <svg width="500" height="200">
+                    <line onClick={this.handleClick.bind(this)}
+                          x1={this.props.x1}
+                          x2={this.props.x2}
+                          y1={this.props.y1}
+                          y2={this.props.y2}
+                          stroke={this.props.stroke}
+                          strokeWidth={this.props.strokeWidth}
+                        />
+                </svg>
+            </div>
         )
     }
 }
--- app/components/d3/d3Container.js
+++ app/components/d3/d3Container.js
@@ -9,19 +9,24 @@ class D3Container extends React.Component {
 
         let dataset = [5, 15, 40, 60, 75, 90];
 
-        let w = 400, h = 50;
+        let w = 400, h = 100;
 
         let svg = d3.select(".d3-container").append("svg").attr("width", w).attr("height", h);
 
         svg.append("line").
             attr({
-                x1: 0, x2: w,
+                x1: 0, x2: 0,
                 y1: 20, y2: 20,
                 "id": "line"
             })
             .style({
                 stroke: "black",
                 "stroke-width": 10
+            })
+            .transition()
+            .duration(2000)
+            .attr({
+                x2: w
             });
 
         svg.selectAll("circle")
@@ -34,7 +39,14 @@ class D3Container extends React.Component {
                     r: 10,
                     "class": "circle"
                 })
-            .style("fill", "orange");
+            .style("fill", "orange")
+            .on("click", function() {
+                d3.select(this).transition().attr({
+                    r: 15
+                })
+                .ease("elastic")
+                .transition().attr({r:10})
+        })
     }
 
     render() {
@@ -42,19 +54,16 @@ class D3Container extends React.Component {
         return (
             <div className="container d3-container">
                 <h1>D3 Action</h1>
-
                 <Line
                     x1="20" x2="100"
                     y1="100" y2="20"
                     stroke="black"
                     strokeWidth="10" />
-
             </div>
-
-
         )
     }
 
 }
 
 module.exports = D3Container;
+
